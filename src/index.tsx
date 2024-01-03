e887e081bcb5b4a57e022fbb63c59b7962abdad4
nareshb7 <nareshbjava7@gmail.com>
2024-01-03 11:57:07 +0530

    index.tsx file added

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(
+        <App />
+    );
+}

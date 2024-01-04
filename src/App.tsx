40508f52a7315c8b88052abc4862a01231f9bc07
Aneesh patil <aneeshpatil341@gmail.com>
2024-01-04 07:31:58 +0530

    eslintconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Homepage';
-import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/login/:name" element={<Login/>}></Route> 
-    </Routes>
-  </BrowserRouter>
+    
     </div>
   );
 }

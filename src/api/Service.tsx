7b8fbbdb4d583e3d6355e9a23874a2d4db890edb
nareshb7 <nareshbjava7@gmail.com>
2024-01-04 18:52:23 +0530

    service file updated

--- src/api/Service.tsx
+++ src/api/Service.tsx
@@ -9,6 +9,7 @@ async function post<T>(path: string, data: T): Promise<Response> {
       },
       body: JSON.stringify(data),
     });
+    console.log('eeee')
     const result = await response.json();
     if (response.status > 399) {
       throw new Error(result.error);

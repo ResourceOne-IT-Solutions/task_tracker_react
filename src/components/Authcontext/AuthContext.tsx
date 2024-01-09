aa7c7e530d12a9a16d5bebb2ffebdef11d8cdb8f
nareshb7 <nareshbjava7@gmail.com>
2024-01-09 13:39:03 +0530

    socket add , dependecey issue resolve

--- src/components/Authcontext/AuthContext.tsx
+++ src/components/Authcontext/AuthContext.tsx
@@ -1,10 +1,14 @@
 import React, { createContext, useContext, useState } from "react";
+import { Socket, io } from "socket.io-client";
+import { BE_URL } from "../../utils/Constants";
 
+const socket = io(BE_URL);
 const UserContext = createContext<UserContext | null>(null);
 export interface UserContext {
   isLoggedin: boolean;
   currentUser: UserModal;
   setCurrentUser: React.Dispatch<React.SetStateAction<UserModal>>;
+  socket: Socket;
 }
 export interface UserModal {
   firstName: string;
@@ -41,6 +45,7 @@ const AuthContext = ({ children }: AuthContextProps) => {
     isLoggedin: (currentUser as UserModal).lastActive ? true : false,
     currentUser,
     setCurrentUser,
+    socket,
   };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };

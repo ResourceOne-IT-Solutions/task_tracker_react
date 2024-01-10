763f2c0623cee46ab292664c2f0c34152a85c1b3
nareshb7 <nareshbjava7@gmail.com>
2024-01-10 15:40:43 +0530

    lint

--- src/utils/Util.tsx
+++ src/utils/Util.tsx
@@ -1,29 +1,29 @@
 export const setCookie = (cvalue: string, exdays: number) => {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie =
-    "presentTaskUser" + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    const expires = "expires=" + d.toUTCString();
+    document.cookie =
+        "presentTaskUser" + "=" + cvalue + ";" + expires + ";path=/";
 };
 
 export const cookieComp = (): string => {
-  const getCookie = (cname: string) => {
-    let totalCookie = document.cookie.split(";");
-    let ca = totalCookie.find((val) => val.includes(cname)) as string;
-    if (ca) {
-      const cv = ca.split("=");
-      return cv[1];
-    }
-    return null;
-  };
-  const checkCookie = () => {
-    let user = getCookie("presentTaskUser");
-    if (user) {
-      return user;
-    } else {
-      setCookie("63daa3b51d791ebc79fdff21db51", 2);
-      return "";
-    }
-  };
-  return checkCookie();
+    const getCookie = (cname: string) => {
+        const totalCookie = document.cookie.split(";");
+        const ca = totalCookie.find((val) => val.includes(cname)) as string;
+        if (ca) {
+            const cv = ca.split("=");
+            return cv[1];
+        }
+        return null;
+    };
+    const checkCookie = () => {
+        const user = getCookie("presentTaskUser");
+        if (user) {
+            return user;
+        } else {
+            setCookie("63daa3b51d791ebc79fdff21db51", 2);
+            return "";
+        }
+    };
+    return checkCookie();
 };

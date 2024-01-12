f68c50a01bf4456ed62ad2d160fdbd26f2e64467
nareshb7 <nareshbjava7@gmail.com>
2024-01-12 15:20:38 +0530

    removed consoles

--- src/pages/dashboard/clientDashboard/index.tsx
+++ src/pages/dashboard/clientDashboard/index.tsx
@@ -4,7 +4,6 @@ import { useLocation, useParams } from "react-router-dom";
 const ClientDashboard = () => {
   const { id } = useParams();
   const { state } = useLocation();
-  console.log("CLIENT::", { id, state });
   return (
     <>
       <h3>CLIENT DASHBOARD</h3>

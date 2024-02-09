6e1fa0b973f03f822023fa00a67e9086e672aa91
nareshb7 <nareshbjava7@gmail.com>
2024-02-09 23:24:38 +0530

    react scripts ejected

---
+++ config/jest/cssTransform.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// This is a custom Jest transformer turning style imports into empty objects.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process(src, filename, config, options) {
+    return {
+      code: 'module.exports = {};',
+    };
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return 'cssTransform';
+  },
+};

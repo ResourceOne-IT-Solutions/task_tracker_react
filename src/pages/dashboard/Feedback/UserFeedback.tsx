917dc6041f7a9daa78c06bec442aaafd26b27115
Aneesh patil <aneeshpatil341@gmail.com>
2024-02-15 14:34:38 +0530

    feedbacklatest

--- src/pages/dashboard/Feedback/UserFeedback.tsx
+++ src/pages/dashboard/Feedback/UserFeedback.tsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
 import httpMethods from "../../../api/Service";
 import TaskTable, { TableHeaders } from "../../../utils/table/Table";
-import {
-  getFormattedDate,
-  getFormattedTime,
-  getFullName,
-} from "../../../utils/utils";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { getFormattedDate, getFormattedTime } from "../../../utils/utils";
+import { Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { useUserContext } from "../../../components/Authcontext/AuthContext";
+import { Severity } from "../../../utils/modal/notification";
+import { UserContext } from "../../../modals/UserModals";
 
 const UserFeedback = () => {
+  const { alertModal } = useUserContext() as UserContext;
   const [userFeedbacks, setUserFeedbacks] = useState([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [selectedFeedback, setSelectedFeedback] = useState<any>(null);
+  const [showFeedbackTable, setshowFeedbackTable] = useState<boolean>(true);
   useEffect(() => {
-    httpMethods.get("/users/feedback").then((res: any) => {
-      setUserFeedbacks(res);
-    });
+    setLoading(true);
+    httpMethods
+      .get("/users/feedback")
+      .then((res: any) => {
+        setUserFeedbacks(res);
+      })
+      .catch((err: any) => {
+        alertModal({
+          severity: Severity.ERROR,
+          content: err.message,
+          title: "Feedback Alert",
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
   const handleDetailClick = (feedback: any) => {
     setSelectedFeedback(feedback);
+    setshowFeedbackTable(false);
   };
   const ticketTableHeaders: TableHeaders<any>[] = [
     { title: "Sl. No", key: "serialNo" },
@@ -54,16 +69,23 @@ const UserFeedback = () => {
       key: "files",
       tdFormat: (feedback) =>
         feedback.files && feedback.files.length > 0 ? (
-          <img
-            style={{
-              width: "100px",
-              height: "100px",
-              cursor: "pointer",
-              position: "relative",
-            }}
-            src={`data:${feedback.files[0].mimetype};base64,${feedback.files[0].buffer}`}
-            alt="Feedback Image"
-          />
+          <>
+            <img
+              style={{
+                width: "100px",
+                height: "100px",
+                cursor: "pointer",
+                position: "relative",
+              }}
+              src={`data:${feedback.files[0].mimetype};base64,${feedback.files[0].buffer}`}
+              alt="Feedback Image"
+            />
+            <p>
+              {feedback.files.length > 1 && (
+                <b>... {feedback.files.length - 1}more</b>
+              )}
+            </p>
+          </>
         ) : (
           <span>No image</span>
         ),
@@ -94,28 +116,66 @@ const UserFeedback = () => {
   ];
   return (
     <div className="user-feedback">
-      <h1 className="text-center">User Feedbacks</h1>
-      <TaskTable<any>
-        pagination
-        headers={ticketTableHeaders}
-        tableData={userFeedbacks}
-        loading={loading}
-      />
-      {selectedFeedback && (
+      {showFeedbackTable && (
+        <>
+          <h1 className="text-center">User Feedbacks</h1>
+          <TaskTable<any>
+            pagination
+            headers={ticketTableHeaders}
+            tableData={userFeedbacks}
+            loading={loading}
+          />
+        </>
+      )}
+      {selectedFeedback && showFeedbackTable == false && (
         <div className="selected-feedback-details mx-auto w-75">
-          <h2 className="text-center">Selected Feedback Details</h2>
-          <p>{selectedFeedback.sender.name}</p>
-          <p>{selectedFeedback.content}</p>
-          {selectedFeedback.files && selectedFeedback.files.length > 0 && (
-            <img
-              style={{
-                width: "500px",
-                height: "500px",
-                position: "relative",
-              }}
-              src={`data:${selectedFeedback.files[0].mimetype};base64,${selectedFeedback.files[0].buffer}`}
-              alt="Selected Feedback Image"
-            />
+          <Button
+            className="my-4"
+            variant="success"
+            onClick={() => setshowFeedbackTable(true)}
+          >
+            Go To Feedbacks
+          </Button>{" "}
+          <h2 className="text-center">Feedback Description</h2>
+          <p>
+            <b>Name:</b> {selectedFeedback.sender.name}
+          </p>
+          <p>
+            <b>Time:</b> {getFormattedDate(selectedFeedback.time)}{" "}
+            {getFormattedTime(selectedFeedback.time)}
+          </p>
+          <p>
+            <b>Feedback:</b> {selectedFeedback.content}
+          </p>
+          {selectedFeedback.files && selectedFeedback.files.length > 0 ? (
+            <>
+              <p>
+                <b>Images: </b>
+              </p>
+              {selectedFeedback.files.map((feed: any, index: any) => {
+                return (
+                  <div key={index} className="my-3">
+                    <img
+                      className="mx-3"
+                      style={{
+                        width: "80%",
+                        height: "600px",
+                        position: "relative",
+                      }}
+                      src={`data:${feed.mimetype};base64,${feed.buffer}`}
+                      alt="Selected Feedback Image"
+                    />
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              <p>
+                <b>Images: </b>
+              </p>
+              <span>No image</span>
+            </>
           )}
         </div>
       )}

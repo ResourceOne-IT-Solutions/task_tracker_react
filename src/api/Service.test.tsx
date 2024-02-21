4b69b236b1e3b80525c6057a84c7b7fc945ae78e
nareshb7 <nareshbjava7@gmail.com>
2024-02-21 15:23:04 +0530

    lint

--- src/api/Service.test.tsx
+++ src/api/Service.test.tsx
@@ -1,13 +1,13 @@
-import httpMethods from './Service';
+import httpMethods from "./Service";
 
-describe('API service functions', () => {
+describe("API service functions", () => {
   beforeEach(() => {
     // Clear any existing mocks before each test
     jest.clearAllMocks();
   });
 
-  it('should make a POST request and return data', async () => {
-    const postData = { name: 'Naresh' };
+  it("should make a POST request and return data", async () => {
+    const postData = { name: "Naresh" };
     const responseData = { name: "Naresh Goud" };
 
     // Mock fetch function to return response data
@@ -16,35 +16,35 @@ describe('API service functions', () => {
       json: () => Promise.resolve(responseData),
     });
 
-    const response = await httpMethods.post('/path', postData);
+    const response = await httpMethods.post("/path", postData);
 
-    expect(fetch).toHaveBeenCalledWith(expect.stringContaining('/path'), {
-      method: 'POST',
+    expect(fetch).toHaveBeenCalledWith(expect.stringContaining("/path"), {
+      method: "POST",
       headers: expect.any(Object),
       body: JSON.stringify(postData),
     });
     expect(response).toEqual(responseData);
   });
-//   it('should make a POST request and return error', async () => {
-//     const postData = { name: 'Naresh' };
-//     const responseData = "Naresh Goud";
+  //   it('should make a POST request and return error', async () => {
+  //     const postData = { name: 'Naresh' };
+  //     const responseData = "Naresh Goud";
 
-//     // Mock fetch function to return response data
-//     global.fetch = jest.fn().mockResolvedValue({
-//       ok: true,
-//       json: () => Promise.reject(responseData),
-//     });
+  //     // Mock fetch function to return response data
+  //     global.fetch = jest.fn().mockResolvedValue({
+  //       ok: true,
+  //       json: () => Promise.reject(responseData),
+  //     });
 
-//     const response = await httpMethods.post('/get-user', postData);
+  //     const response = await httpMethods.post('/get-user', postData);
 
-//     expect(fetch).toHaveBeenCalledWith(expect.stringContaining('/get-user'), {
-//       method: 'POST',
-//       headers: expect.any(Object),
-//       body: JSON.stringify(postData),
-//     });
-//     console.log('RES::', response)
-//     expect(response).toEqual(responseData);
-//   });
+  //     expect(fetch).toHaveBeenCalledWith(expect.stringContaining('/get-user'), {
+  //       method: 'POST',
+  //       headers: expect.any(Object),
+  //       body: JSON.stringify(postData),
+  //     });
+  //     console.log('RES::', response)
+  //     expect(response).toEqual(responseData);
+  //   });
 
   // Write similar tests for other API service functions (get, put, deleteCall)
 });
--- src/components/Authcontext/AuthContext.test.tsx
+++ src/components/Authcontext/AuthContext.test.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { render, act } from '@testing-library/react';
-import AuthContext, { useUserContext } from './AuthContext';
-import { io as mockIo } from 'socket.io-client'; // Mock socket.io-client
-import httpMethods from '../../api/Service';
-import { UserContext } from '../../modals/UserModals';
-import App from '../../App';
-import { BE_URL } from '../../utils/Constants';
+import React from "react";
+import { render, act } from "@testing-library/react";
+import AuthContext, { useUserContext } from "./AuthContext";
+import { io as mockIo } from "socket.io-client"; // Mock socket.io-client
+import httpMethods from "../../api/Service";
+import { UserContext } from "../../modals/UserModals";
+import App from "../../App";
+import { BE_URL } from "../../utils/Constants";
 
-jest.mock('socket.io-client'); // Mock socket.io-client
-jest.mock('../../api/Service'); // Mock httpMethods
+jest.mock("socket.io-client"); // Mock socket.io-client
+jest.mock("../../api/Service"); // Mock httpMethods
 const socket = mockIo(BE_URL);
-describe('AuthContext', () => {
+describe("AuthContext", () => {
   beforeEach(() => {
     // Reset mocks before each test
     jest.clearAllMocks();
   });
 
-  it('should fetch user data and set state on mount', async () => {
+  it("should fetch user data and set state on mount", async () => {
     // const userData = { firstName: "Naresh", lastName: "Baleboina" };
     // const getMock = jest.spyOn(httpMethods, 'get').mockResolvedValue(userData);
-    
+
     // // Render AuthContext component
     // await act(async () => {
     //   render(<AuthContext><App /></AuthContext>);
     // });
 
     // expect(getMock).toHaveBeenCalledWith('/get-user');
-    expect(true).toBe(true)
+    expect(true).toBe(true);
     // const userContext = useUserContext() as UserContext
     // expect(userContext.isLoggedin).toBe(true);
     // expect(userContext.currentUser).toEqual(userData);
@@ -35,4 +35,3 @@ describe('AuthContext', () => {
 
   // Add more test cases for other scenarios, such as error handling, socket events, etc.
 });
-
